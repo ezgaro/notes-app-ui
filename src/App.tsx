a88d3e7203ddbb63ea87eb3620e337447638a07b
ezgaro <ezgaro2005@gmail.com>
2024-11-10 14:50:07 +0200

    Adding new notes through the form

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,36 @@ function App() {
     { id: 3, title: "note 3", content: "content 3" },
     { id: 4, title: "note 4", content: "content 4" },
   ]);
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    setNotes([
+      ...notes,
+      { id: notes.length + 1, title: title, content: content },
+    ]);
+    setTitle("");
+    setContent("");
+  }
+
   return (
     <div className="app-container">
-      <form className="note-form">
-        <input placeholder="Title" required />
-        <textarea placeholder="Content" rows={10} required></textarea>
+      <form className="note-form" onSubmit={handleSubmit}>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+          required
+        />
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Content"
+          rows={10}
+          required
+        ></textarea>
         <button type="submit">Add Note</button>
       </form>
       <div className="notes-grid">
