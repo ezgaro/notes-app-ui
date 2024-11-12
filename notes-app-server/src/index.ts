6f90858df275aaaa1140a5c137a22d6d14f0d585
ezgaro <ezgaro2005@gmail.com>
2024-11-12 18:55:07 +0200

    Create Note end point

--- notes-app-server/src/index.ts
+++ notes-app-server/src/index.ts
@@ -13,4 +13,22 @@ app.get("/api/notes", async (req, res) => {
   res.json(notes);
 });
 
+import { Request, Response } from "express";
+
+app.post("/api/notes", async (req, res) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).send("title and content fields required");
+  }
+  try {
+    const note = await prisma.note.create({
+      data: { title, content },
+    });
+    res.json(note);
+  } catch (error) {
+    res.status(400).send("title and content fields required");
+  }
+});
+
 app.listen(5000, () => console.log("Server is running on port 5000"));
